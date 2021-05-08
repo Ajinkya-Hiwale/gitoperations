b6364f7bbb66cffd5e07faf9bb28702d6bf905ee
Ajinkya Hiwale <iajinkyahiwale@gmail.com>
2021-05-08 17:33:54 +0530

    file added

---
+++ src/test/java/gitPractice/gitopeartions/testing.java
@@ -0,0 +1,12 @@
+package gitPractice.gitopeartions;
+
+public class testing {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println(20-10);
+		System.out.print(20+10);
+	}
+
+}
